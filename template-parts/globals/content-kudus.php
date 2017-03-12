5d9b9faaa1a260b41a9615ffa6159112e3ca1e98
ido barnea <barbareshet@gmail.com>
2017-03-12 07:09:25 +0200

    kudus slider with slick

--- template-parts/globals/content-kudus.php
+++ template-parts/globals/content-kudus.php
@@ -16,17 +16,24 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-8 col-md-offset-2 col-sm-12 kudus-slider">
+            <div class="col-md-8 col-md-offset-2 col-sm-12 kudus-slider slick-item">
                 <?php
                 $test_p = get_field('testimonial_section_cont');
                 if($test_p):
                 $count = 1;
                 foreach ($test_p as $tp):
+                    ?>
+                    <div class="kudus-cont">
+                             <i class="fa fa-quote-left"></i>
+                             <?php the_field('testimonial_content', $tp->ID) ;?>
+                             <i class="fa fa-quote-right"></i>
+                        <span class="kudus-name">
+                            ~<?php the_field('testimonial_name', $tp->ID);?>
+                        </span>
+                    </div>
 
-                         printf(
-                            esc_html('%1$s %2$s', 'the-towing-theme'), '<div class="kudus-cont"><i class="fa fa-quote-left"></i>'. get_field('testimonial_content', $tp->ID) .'<i class="fa fa-quote-right"></i></div>',
-                            '<span class="kudus-name">~'.get_field('testimonial_name', $tp->ID) .'</span>');
-                $count++;
+          <?php      $count++;
+
                      endforeach;
 
                 endif;
