95d025e12dba8d3815be20b701a045a55d0f0fcb
ido barnea <barbareshet@gmail.com>
2017-03-13 16:16:50 +0200

    schema fixes

--- template-parts/globals/content-footer-widgets.php
+++ template-parts/globals/content-footer-widgets.php
@@ -6,24 +6,30 @@
  * Time: 12:32 PM
  */
 ?>
-<div class="col-md-3" id="footer-widget-1">
-    <?php echo do_shortcode('[localbusiness]');?>
-</div>
-<div class="col-md-3" id="footer-widget-2">
-    <?php if(is_active_sidebar('footer-2')){
-        dynamic_sidebar('footer-2');
-    }
-    ?>
-</div>
-<div class="col-md-3" id="footer-widget-3">
-    <?php if(is_active_sidebar('footer-3')){
-        dynamic_sidebar('footer-3');
-    }
-    ?>
-</div>
-<div class="col-md-3" id="footer-widget-4">
-    <?php if(is_active_sidebar('footer-4')){
-        dynamic_sidebar('footer-4');
-    }
-    ?>
-</div>
+<section id="footer-widgets">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3" id="footer-widget-1">
+                <?php echo do_shortcode('[localbusiness]');?>
+            </div>
+            <div class="col-md-3" id="footer-widget-2">
+                <?php if(is_active_sidebar('footer-2')){
+                    dynamic_sidebar('footer-2');
+                }
+                ?>
+            </div>
+            <div class="col-md-3" id="footer-widget-3">
+                <?php if(is_active_sidebar('footer-3')){
+                    dynamic_sidebar('footer-3');
+                }
+                ?>
+            </div>
+            <div class="col-md-3" id="footer-widget-4">
+                <?php if(is_active_sidebar('footer-4')){
+                    dynamic_sidebar('footer-4');
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</section>
--- template-parts/globals/content-schema.php
+++ template-parts/globals/content-schema.php
@@ -10,13 +10,13 @@
 ?>
 <div id="footer-disclaimer">
 
-    <div itemscope itemtype="http://schema.org/<?php echo get_theme_mod(__('schema_type'));?>">
+    <div itemscope itemtype="http://schema.org/<?php echo get_theme_mod(__('schema_type')) ? get_theme_mod(__('schema_type')) : 'locksmith';?>">
         <ul class="footer-list">
             <li>
                 <div class="footer-logo">
                     <a itemprop="url" href="<?php echo get_home_url(); ?>" alt="<?php echo get_bloginfo('name'); ?>"  title="<?php echo get_bloginfo('name'); ?>">
                         <span itemprop="logo" itemtype="https://schema.org/ImageObject">
-                            <img src="<?php echo get_template_directory_uri() . '/assets/img/logo_small.png' ;?>" alt="<?php echo get_bloginfo('name'); ?>" itemprop="image">
+                            <img src="<?php echo get_theme_mod('schema_logo') ? get_theme_mod('schema_logo') : get_stylesheet_directory_uri() . '/assets/img/logo.png' ;?>" alt="<?php echo get_bloginfo('name'); ?>" itemprop="image">
                         </span>
                     </a>
                 </div>
@@ -24,45 +24,40 @@
 
             <li>
                 <div class="footer-company-info">
-                    <span itemprop="name"><?php echo get_theme_mod(__('schema_brand_name')); ?></span><br>
-                    <span itemprop="description"><?php echo get_theme_mod(__('schema_brand_description')); ?></span>
+                    <span itemprop="name"><?php echo get_theme_mod(__('schema_brand_name')) ? get_theme_mod(__('schema_brand_name')) : get_bloginfo('name'); ?></span><br>
+                    <span itemprop="description"><?php echo get_theme_mod(__('schema_brand_description')) ? get_theme_mod(__('schema_brand_description')) : get_bloginfo('description'); ?></span>
                 </div>
             </li>
             <li class="inline-block">
                 <div class="footer-address">
                     <div class="description" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
                         <i class="fa fa-home">&nbsp</i>
-                        <span itemprop="streetAddress"><?php echo get_theme_mod(__('schema_street_address')); ?></span>,<br>
-                        <span itemprop="addressLocality"><?php echo get_theme_mod(__('schema_city')); ?>, </span>
-                        <span itemprop="addressRegion"><?php echo get_theme_mod(__('schema_region')); ?>, </span>
-                        <span itemprop="postalCode"><?php echo get_theme_mod(__('schema_zip')); ?></span>
+                        <span itemprop="streetAddress"><?php echo get_theme_mod(__('schema_street_address')) ? get_theme_mod(__('schema_street_address')) : __('Add street name'); ?></span>,<br>
+                        <span itemprop="addressLocality"><?php echo get_theme_mod(__('schema_city')) ? get_theme_mod(__('schema_city')) : __('Add city name'); ?>, </span>
+                        <span itemprop="addressRegion"><?php echo get_theme_mod(__('schema_region')) ? get_theme_mod(__('schema_region')) : __('Add region'); ?>, </span>
+                        <span itemprop="postalCode"><?php echo get_theme_mod(__('schema_zip')) ? get_theme_mod(__('schema_zip')) : __('Add zip code'); ?></span>
                     </div>
                 </div>
             </li>
             <li class="inline-block">
                 <div class="footer-phone"><i class="fa fa-phone">&nbsp</i>
-                    <span itemprop="telephone"><?php echo get_theme_mod('schema_phone_number');?></span>
+                    <span itemprop="telephone"><?php echo get_theme_mod('schema_phone_number') ? get_theme_mod('schema_phone_number') : __('Add phone number');?></span>
 
                 </div>
             </li>
             <li class="inline-block">
-                <div class="footer-hours"><i class="fa fa-clock-o">&nbsp</i>
-                    <?php if(have_rows('company_opening_hours','options') ):
-                        while( have_rows('company_opening_hours','options')): the_row();?>
-                            <time itemprop="openingHours" datetime="<?php the_sub_field('opening_hours_day','options');?>"><?php the_sub_field('opening_hours_time','options');?></time>
-                        <?php endwhile; endif;?>
+                <div class="footer-social-icons">
+                    <ul class="list-inline">
+                        <li><a href="<?php the_field('company_fb','options');?>" target="_blank" rel="nofollow"><i class="fa fa-facebook"></i> </a></li>
+                        <li><a href="<?php the_field('company_twitter','options');?>" target="_blank" rel="nofollow"><i class="fa fa-twitter"></i> </a></li>
+                        <li><a href="<?php the_field('company_gmb','options');?>" target="_blank" rel="nofollow"><i class="fa fa-google-plus"></i> </a></li>
+                        <li><a href="<?php the_field('company_yelp','options');?>" target="_blank" rel="nofollow"><i class="fa fa-yelp"></i> </a></li>
+                    </ul>
                 </div>
             </li>
         </ul>
 
-<!--        <div class="footer-social-icons">-->
-<!--            <ul class="list-inline">-->
-<!--                <li><a href="--><?php //the_field('company_fb','options');?><!--" target="_blank" rel="nofollow"><i class="fa fa-facebook"></i> </a></li>-->
-<!--                <li><a href="--><?php //the_field('company_twitter','options');?><!--" target="_blank" rel="nofollow"><i class="fa fa-twitter"></i> </a></li>-->
-<!--                <li><a href="--><?php //the_field('company_gmb','options');?><!--" target="_blank" rel="nofollow"><i class="fa fa-google-plus"></i> </a></li>-->
-<!--                <li><a href="--><?php //the_field('company_yelp','options');?><!--" target="_blank" rel="nofollow"><i class="fa fa-yelp"></i> </a></li>-->
-<!--            </ul>-->
-<!--        </div>-->
+
 
 
         <div class="clear"></div>
