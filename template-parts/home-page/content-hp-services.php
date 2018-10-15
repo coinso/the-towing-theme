c8a5ab67e519fcb7b10e584166624ca020671d2d
ido barnea <barbareshet@gmail.com>
2018-10-15 15:40:11 +0300

    hp service cards better transition

--- template-parts/home-page/content-hp-services.php
+++ template-parts/home-page/content-hp-services.php
@@ -13,15 +13,15 @@ if( $services ):
         <hr class="divider">
         <div class="row">
             <?php foreach( $services as $service ):
+                $service_name = get_field('service_name', $service->ID) ? get_field('service_name', $service->ID) : get_the_title( $service->ID );
                 ?>
                 <div class="service-card panel panel-default col-md-3 col-sm-6 col-xs-6">
                     <a href="<?php echo get_permalink( $service->ID ); ?>">
                         <?php $photo = get_field('service_image', $service->ID);?>
-                        <img src="<?php echo $photo['url']; ?>" alt="" />
+                        <img src="<?php echo $photo['url']; ?>" alt="<?php echo $photo['alt']; ?>" class="service-card__img"/>
                         <div class="overlay"></div>
-
                         <h3 class="card-title">
-                            <?php echo get_the_title( $service->ID ); ?>
+                            <?php echo esc_html( $service_name ); ?>
                             <br>
                             <i class="fa fa-caret-down service-card-read-more"></i>
                         </h3>
